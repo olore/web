24e6d1b29439b45249f8ab1370a0729601ab3238
Brian Olore <brian@olore.net>
2020-05-05 09:39:00 -0400

    somewhat functional, with ugly type hack
    Signed-off-by: Brian Olore <brian@olore.net>

--- src/components/log/QueryTable.tsx
+++ src/components/log/QueryTable.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
-import { useTable } from 'react-table';
+import { useTable, usePagination, UsePaginationOptions, UseSortByOptions, IdType } from 'react-table';
+
+// export interface TableInstance<D extends object = {}>
+//     extends UseFiltersInstanceProps<D>,
+//       UseTableHeaderGroupProps<D>,
+//       UseTableInstanceProps<D>,
+//       UseFiltersInstanceProps<D>,
+//       UsePaginationInstanceProps<D> {}
+
+export interface TableOptions<D extends object>
+  extends UsePaginationOptions<D>,
+    UseSortByOptions<D> {};
+
+export interface TableState<D extends object = {}> {
+    hiddenColumns?: Array<IdType<D>>;
+}
 
 export default function QueryTable({ columns, data}: any) {
 
@@ -10,16 +25,72 @@ export default function QueryTable({ columns, data}: any) {
     headerGroups,
     rows,
     prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
   } = useTable({
     columns,
     data,
-  })
+    initialState: { pageIndex: 0, pageSize: 25 },
+  }, usePagination)
 
   // Render the UI for your table
   return (
+    <>
+    <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
     <table {...getTableProps()}>
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup: { getHeaderGroupProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableRowElement> & React.HTMLAttributes<HTMLTableRowElement>; headers: any[]; }) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
               <th {...column.getHeaderProps()}>{column.render('Header')}</th>
@@ -28,7 +99,7 @@ export default function QueryTable({ columns, data}: any) {
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
+        {page.map((row: { getRowProps: () => JSX.IntrinsicAttributes & React.ClassAttributes<HTMLTableRowElement> & React.HTMLAttributes<HTMLTableRowElement>; cells: any[]; }, i: any) => {
           prepareRow(row)
           return (
             <tr {...row.getRowProps()}>
@@ -40,5 +111,6 @@ export default function QueryTable({ columns, data}: any) {
         })}
       </tbody>
     </table>
+    </>
   )
 }
