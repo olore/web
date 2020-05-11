1289a932fefc5ec6949b3b9ed484e9b7225aad9e
Brian Olore <brian@olore.net>
2020-05-11 10:05:58 -0400

    style the pagination
    Signed-off-by: Brian Olore <brian@olore.net>

--- src/components/log/QueryPagination.tsx
+++ src/components/log/QueryPagination.tsx
@@ -14,52 +14,64 @@ export default function QueryPagination({ data }: any) {
   } = data;
 
   return (
-    <div className="pagination">
+    <div
+      className="pagination w-100 d-flex flex-row p-1 bg-white"
+      style={{ alignItems: "stretch", justifyContent: "space-between" }}
+    >
       <button
-        style={{ width: 200 }}
-        className="p-2 m-3"
         onClick={() => previousPage()}
         disabled={!canPreviousPage}
+        style={{ flex: "1 1" }}
       >
         Previous
       </button>
 
-      <span className="p-2 m-3">
-        <input
-          type="number"
-          value={pageIndex + 1}
-          className="text-center mr-1"
+      <div
+        className="d-flex text-center flex-row flex-wrap"
+        style={{
+          flex: "1.5 1",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <span className="py-1 px-3">
+          <span className="px-1">Page</span>
+          <input
+            type="number"
+            value={pageIndex + 1}
+            className="text-center mr-1"
+            style={{ width: 70 }}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+          />
+          of {pageOptions.length}
+        </span>
+
+        <select
+          className="bg-white"
+          style={{ borderRadius: 3 }}
+          value={pageSize}
           onChange={e => {
-            const page = e.target.value ? Number(e.target.value) - 1 : 0;
-            gotoPage(page);
+            setPageSize(Number(e.target.value));
           }}
-          style={{ width: "70px" }}
-        />
-        of {pageOptions.length}
-      </span>
+        >
+          {[10, 25, 50, 100].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
 
       <button
-        style={{ width: 200 }}
-        className="p-2 m-3"
         onClick={() => nextPage()}
         disabled={!canNextPage}
+        style={{ flex: "1 1" }}
       >
         Next
       </button>
-
-      <select
-        style={{ maxHeight: 25, marginTop: 15 }}
-        value={pageSize}
-        onChange={e => {
-          setPageSize(Number(e.target.value));
-        }}
-      >
-        {[10, 25, 50, 100].map(pageSize => (
-          <option key={pageSize} value={pageSize}>
-            Show {pageSize}
-          </option>
-        ))}
-      </select>
     </div>
   );
 }
